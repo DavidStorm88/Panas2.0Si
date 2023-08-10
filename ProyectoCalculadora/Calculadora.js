50573672a2f45a32adb6e7a14ae44df6211f1f9f
AJAC-2023 <aj.amagua@itq.edu.ec>
2023-08-10 11:29:27 -0500

    Establecer espacios

--- ProyectoCalculadora/Calculadora.js
+++ ProyectoCalculadora/Calculadora.js
@@ -2,12 +2,15 @@ class Calculadora {
     suma(num1, num2) {
         return num1 + num2;
     }
+
     resta(num1, num2) {
         return num1 - num2;
     }
+
     dividir(num1, num2) {
         return num1 / num2;
     }
+    
     multiplicar(num1, num2) {
         return num1 * num2;
     }
